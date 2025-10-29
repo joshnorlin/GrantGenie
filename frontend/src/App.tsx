75f819b9a98f4b22c5e9edf947497363f3e9025c
josh <jnorlin@outlook.com>
2025-10-28 23:27:03 -0700

    feat(auth): implement supabase's auth guide

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,32 @@
-function App() {
-  return (
-    <>
-      <div>Hello World.</div>
-    </>
-  )
-}
+import { useState, useEffect } from 'react'
+import { createClient, type Session } from '@supabase/supabase-js'
+import { Auth } from '@supabase/auth-ui-react'
+import { ThemeSupa } from '@supabase/auth-ui-shared'
+import { Home } from './Home'
 
-export default App
+const supabase = createClient('https://ihoqewwgkpjmkgwoenck.supabase.co', 'sb_publishable_D5CnwE2fd6yCsARi6MVNGA_C-FVjvSd')
+
+export default function App() {
+  const [session, setSession] = useState<Session | null>(null)
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session)
+    })
+
+    const {
+      data: { subscription },
+    } = supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session)
+    })
+
+    return () => subscription.unsubscribe()
+  }, [])
+
+  if (!session) {
+    return (<Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} />)
+  }
+  else {
+    return (<Home />)
+  }
+}
---
+++ frontend/src/Home.tsx
@@ -0,0 +1,8 @@
+export function Home() {
+  return (
+    <div>
+      <div>Logged in!</div>
+      <button>Hello</button>
+    </div>
+  )
+}
